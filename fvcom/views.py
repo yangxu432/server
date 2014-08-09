46ec8aed075fa012109b2346db755a7c90ef8d3e
thinkpad-ubuntu-server <yangxu432@gmail.com>
2014-08-09 19:10:34 -0400

    setup

--- fvcom/views.py
+++ fvcom/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 # Create your views here.
+
+def index(request):
+   ctx = {'value':'values'} 
+   return render_to_response('index.html', ctx, context_instance=RequestContext(request))
+
+
+
